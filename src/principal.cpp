3dba9b1700deefbcb3b711fffe0c2236b495bde7
alexFiorenza <alexfiorenza2012@gmail.com>
2022-05-06 09:38:50 -0300

    Initialization of api

---
+++ src/principal.cpp
@@ -0,0 +1,22 @@
+
+#ifndef _MAIN
+#define _MAIN
+
+#include <iostream>
+#include "biblioteca/funciones/strings.hpp"
+#include "biblioteca/funciones/tokens.hpp"
+#include "biblioteca/funciones/files.hpp"
+#include "biblioteca/tads/Array.hpp"
+#include "biblioteca/tads/Map.hpp"
+#include "biblioteca/tads/List.hpp"
+#include "biblioteca/tads/Stack.hpp"
+#include "biblioteca/tads/Queue.hpp"
+#include "biblioteca/tads/Coll.hpp"
+using namespace std;
+
+int main()
+{
+   return 0;
+}
+
+#endif
