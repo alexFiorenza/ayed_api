852a4250143ba665c7a65fa2a3b94487acedb680
Alex Fiorenza <alexfiorenza2012@gmail.com>
2022-11-17 09:02:05 -0300

    Updated all tads

--- src/biblioteca/tads/Queue.hpp
+++ src/biblioteca/tads/Queue.hpp
@@ -1,46 +1,57 @@
 #ifndef _TQUEUE_TAD_
 #define _TQUEUE_TAD_
 
-#include <iostream>
 #include "../funciones/lists.hpp"
+#include <iostream>
 
 using namespace std;
 
-template<typename T>
+template <typename T>
 struct Queue
 {
+   Node<T> *p;
+   Node<T> *q;
 };
 
-template<typename T>
+template <typename T>
 Queue<T> queue()
 {
    Queue<T> q;
+   q.p = NULL;
+   q.q = NULL;
    return q;
 }
 
-template<typename T>
-T* queueEnqueue(Queue<T>& q,T e)
+template <typename T>
+T *queueEnqueue(Queue<T> &q, T e)
 {
-   return NULL;
+   Node<T> *n = enqueue(q.p, q.q, e);
+   return &(n->d);
 }
 
-template<typename T>
-T queueDequeue(Queue<T>& q)
+template <typename T>
+T queueDequeue(Queue<T> &q)
 {
-   T t;
-   return t;
+   return dequeue(q.p, q.q);
 }
 
-template<typename T>
+template <typename T>
 bool queueIsEmpty(Queue<T> q)
 {
-   return true;
+   return q.p == NULL;
 }
 
-template<typename T>
+template <typename T>
 int queueSize(Queue<T> q)
 {
-   return 0;
+   int i = 0;
+   Node<T> *aux = q.p;
+   while (aux != NULL)
+   {
+      i++;
+      aux = aux->n;
+   }
+   return i;
 }
 
 #endif
